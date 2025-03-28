daa07a1ff6bf05c675d98e2b2db919c1b120d796
kleber <muruchi2018@gmail.com>
2025-03-28 12:46:19 -0400

    pagina principal

---
+++ resources/views/layout/header.blade.php
@@ -0,0 +1,13 @@
+<header>
+    <nav>
+        <div class="logo">OH! <span>SANSI</span></div>
+        <div class="nav-links">
+            <a href="#">Inicio</a>
+            <a href="#">Comvocatoria</a>
+            <a href="#">Reglamento</a>
+        </div>
+        <div class="auth-buttons">
+            <a href="#" class="join-btn" id="login-button">Iniciar Sesión</a>
+        </div>
+    </nav>
+</header>
